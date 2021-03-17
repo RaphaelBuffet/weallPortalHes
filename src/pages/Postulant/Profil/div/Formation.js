2c3dffb9d6fb194199b31870df8c48e28980a5f5
Raphael.Buffet <lokiraphael@hotmail.com>
2021-03-17 17:44:13 +0100

    formation finis

--- src/pages/Postulant/Profil/div/Formation.js
+++ src/pages/Postulant/Profil/div/Formation.js
@@ -1,21 +1,67 @@
 import React from 'react'
 import {AccordionSecondary} from '../../../../components/Accordion/Accordion'
 import './form.css'
+import {data} from './data'
 var formation = [];
 export default class Formation extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            length: 2
-        };
-        this.addFormation = this.addFormation.bind(this);
+            formation: data.formation
+          };
+          // changement des input
+    this.handleDateDebut = this.handleDateDebut.bind(this);
+    this.handleDateFin = this.handleDateFin.bind(this);
+    this.handleCursus = this.handleCursus.bind(this);
+    this.handleInstitut = this.handleInstitut.bind(this);
+    this.handleDegreEtude = this.handleDegreEtude.bind(this);
+    this.handleDiplome = this.handleDiplome.bind(this);
+    // gestion des actions des bouttons
+    this.handleEnregistrer = this.handleEnregistrer.bind(this);
+    this.addFormation = this.addFormation.bind(this);
     }
+    handleDateDebut(index,value) {
+        this.state.formation[index].debut=value.target.value
+        this.forceUpdate()
+      }
+      handleDateFin(index,value) {
+        this.state.formation[index].fin=value.target.value
+        this.forceUpdate()
+      }
+      handleCursus(index,value) {
+        this.state.formation[index].cursus=value.target.value
+        this.forceUpdate()
+      }
+      handleInstitut(index,value) {
+        this.state.formation[index].institut=value.target.value
+        this.forceUpdate()
+      }
+      handleDegreEtude(index,value) {
+        this.state.formation[index].degre=value.target.value
+        this.forceUpdate()
+      }
+      handleDiplome(index,value) {
+        this.state.formation[index].diplome=value.target.value
+        this.forceUpdate()
+      }
     addFormation() {
-        this.setState({ length: this.state.length + 1 });
+        let newformation=this.state.formation
+        newformation.push({
+            debut:'2021-01-01',
+            fin:'2021-01-01',
+            cursus: 0,
+            institut: 0,
+            degre: 0,
+            diplome: ''
+        })
+        this.setState({ formation: newformation });
+    }
+    handleEnregistrer(){
+        alert('La formation est soumis');
     }
     render() {
         formation = []
-        for (let i = 0; i < this.state.length; i++) {
+        for (let i = 0; i < this.state.formation.length; i++) {
             formation.push(
                 <div key={i}>
                     <AccordionSecondary title={"Formations "+(i+1)} className='accordion-secondary'>
@@ -23,24 +69,24 @@ export default class Formation extends React.Component {
                             <div className='line-half'>
                                 <div className='column'>
                                     <p className='intitulé'>Date de début</p>
-                                    <input type="date" value={this.state.naissance} onChange={this.handleNaissance} className='input' />
+                                    <input type="date" value={this.state.formation[i].debut} onChange={(value)=>this.handleDateDebut(i,value)} className='input' />
                                 </div>
                                 <div className='column'>
                                     <p className='intitulé'>Date de fin</p>
-                                    <input type="date" value={this.state.naissance} onChange={this.handleNaissance} className='input' />
+                                    <input type="date" value={this.state.formation[i].fin} onChange={(value)=>this.handleDateFin(i,value)} className='input' />
                                 </div>
                             </div>
                             <div className='line'>
                                 <div className='column'>
                                     <p className='intitulé'>Cursus</p>
-                                    <select value={this.state.sexe} onChange={this.handleSexe} className='input'>
+                                    <select value={this.state.formation[i].cursus}onChange={(value)=>this.handleCursus(i,value)} className='input'>
                                         <option value="0">Universiatire</option>
                                         <option value="1">Haute école spécialisée</option>
                                     </select>
                                 </div>
                                 <div className='column'>
                                     <p className='intitulé'>Institut</p>
-                                    <select value={this.state.sexe} onChange={this.handleSexe} className='input'>
+                                    <select value={this.state.formation[i].institut} onChange={(value)=>this.handleInstitut(i,value)} className='input'>
                                         <option value="0">Informatique de gestion</option>
                                         <option value="1">Tourisme</option>
                                         <option value="2">Economie et Service</option>
@@ -50,18 +96,14 @@ export default class Formation extends React.Component {
                             <div className='line'>
                                 <div className='column'>
                                     <p className='intitulé'>Degré d'étude</p>
-                                    <select value={this.state.sexe} onChange={this.handleSexe} className='input'>
-                                        <option value="0">Universiatire</option>
-                                        <option value="1">Haute école spécialisée</option>
+                                    <select value={this.state.formation[i].degre} onChange={(value)=>this.handleDegreEtude(i,value)} className='input'>
+                                        <option value="0">Bachelor</option>
+                                        <option value="1">Master</option>
                                     </select>
                                 </div>
                                 <div className='column'>
                                     <p className='intitulé'>Nom du diplôme</p>
-                                    <select value={this.state.sexe} onChange={this.handleSexe} className='input'>
-                                        <option value="0">Informatique de gestion</option>
-                                        <option value="1">Tourisme</option>
-                                        <option value="2">Economie et Service</option>
-                                    </select>
+                                    <input type="text" value={this.state.formation[i].diplome} onChange={(value)=>this.handleDiplome(i,value)} className='input' />
                                 </div>
                             </div>
                             <input type="submit" value="enregistrer les informations" className='btn-enregistrer' />
--- src/pages/Postulant/Profil/div/data.js
+++ src/pages/Postulant/Profil/div/data.js
@@ -8,5 +8,23 @@ export const data = {
     descritpion: 'coucou je sui un test',
     npa: '1967',
     localité: '0',
-    telephone: '078 935 47 75'
+    telephone: '078 935 47 75',
+    formation :[
+        {
+            debut:'2017-09-23',
+            fin:'2021-03-01',
+            cursus: 1,
+            institut: 1,
+            degre: 1,
+            diplome: 'informatique de gestion'
+        },
+        {
+            debut:'2012-08-01',
+            fin:'2016-07-30',
+            cursus: 1,
+            institut: 1,
+            degre: 1,
+            diplome: 'Maturité commerciale'
+        }
+    ]
 }
