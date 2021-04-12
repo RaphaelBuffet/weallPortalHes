15278cf8ef63b60bde47a0445987b0499641be8e
sam119 <samuel.pintodasilva@students.hevs.ch>
2021-04-12 16:57:00 +0200

    First modifications (colors principally)

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -16,10 +16,11 @@ export const Button = ({
     onClick,
     buttonStyle,
     buttonSize
+
 }) => {
     const checkButtonStyle =STYLES.includes(buttonStyle) ?buttonStyle :STYLES[0]
-
     const checkButtonSize =SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+
 
     return (
      <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick}type={type}>
